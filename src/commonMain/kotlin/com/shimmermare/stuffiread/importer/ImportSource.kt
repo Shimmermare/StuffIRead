5cdc57d17c27c85917244df2b64432641c5a1683
Denis Pavlov <shimmermare@gmail.com>
2023-04-12 21:30:15 +0300

    Add more settings: update checks, showing foreign sites in import

--- src/commonMain/kotlin/com/shimmermare/stuffiread/importer/ImportSource.kt
+++ src/commonMain/kotlin/com/shimmermare/stuffiread/importer/ImportSource.kt
@@ -8,17 +8,20 @@ import com.shimmermare.stuffiread.importer.ponebin.PonebinImporter
 import com.shimmermare.stuffiread.importer.ponebin.PonebinUrlParser
 import com.shimmermare.stuffiread.importer.ponepaste.PonepasteImporter
 import com.shimmermare.stuffiread.importer.ponepaste.PonepasteUrlParser
+import de.comahe.i18n4k.Locale
+import de.comahe.i18n4k.forLocaleTag
 
 enum class ImportSource(
     val urlParser: UrlParser<*>,
     val importer: StoryImporter<out ImportSettings>,
     val ponyIntegration: Boolean = false,
+    val locale: Locale,
 ) {
-    ARCHIVE_OF_OUR_OWN(ArchiveOfOurOwnUrlParser, ArchiveOfOurOwnImporter),
-    PASTEBIN(PastebinUrlParser, PastebinImporter),
-    PONEPASTE(PonepasteUrlParser, PonepasteImporter, ponyIntegration = true),
-    PONEBIN(PonebinUrlParser, PonebinImporter, ponyIntegration = true),
-    // TODO FIMFICTION(..., ..., ponyIntegration = true),
-    // TODO FICBOOK(..., ...), - also maybe add settings bool to disable non-english sources?
-    // TODO PONYFICTION(PastebinUrlParser, PastebinImporter, ponyIntegration = true),
+    ARCHIVE_OF_OUR_OWN(ArchiveOfOurOwnUrlParser, ArchiveOfOurOwnImporter, locale = forLocaleTag("en")),
+    PASTEBIN(PastebinUrlParser, PastebinImporter, locale = forLocaleTag("en")),
+    PONEPASTE(PonepasteUrlParser, PonepasteImporter, ponyIntegration = true, locale = forLocaleTag("en")),
+    PONEBIN(PonebinUrlParser, PonebinImporter, ponyIntegration = true, locale = forLocaleTag("en")),
+    // TODO FIMFICTION(..., ..., ponyIntegration = true, locale = forLocaleTag("en")),
+    // TODO FICBOOK(..., ..., locale = forLocaleTag("ru")),
+    // TODO PONYFICTION(..., ..., ponyIntegration = true, locale = forLocaleTag("ru")),
 }
--- src/commonMain/kotlin/com/shimmermare/stuffiread/settings/AppSettingsServiceImpl.kt
+++ src/commonMain/kotlin/com/shimmermare/stuffiread/settings/AppSettingsServiceImpl.kt
@@ -8,6 +8,7 @@ import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_CHECK_U
 import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_ENABLE_PONY_INTEGRATIONS
 import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_OPEN_LAST_ARCHIVE_ON_STARTUP
 import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_SCORE_DISPLAY_TYPE
+import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_SHOW_FOREIGN_IMPORT_SOURCES
 import com.shimmermare.stuffiread.settings.AppSettings.Companion.DEFAULT_THEME_BEHAVIOR
 import com.shimmermare.stuffiread.settings.AppSettings.Companion.RECENTLY_OPENED_TO_KEEP
 import com.shimmermare.stuffiread.util.AppJson
@@ -64,12 +65,6 @@ class AppSettingsServiceImpl : AppSettingsService {
                 null
             }
         }
-        val themeBehavior = getFromSourceAndParseOrDefault(THEME_BEHAVIOR_KEY, DEFAULT_THEME_BEHAVIOR) {
-            ThemeBehavior.valueOf(it)
-        }
-        val scoreDisplayType = getFromSourceAndParseOrDefault(SCORE_DISPLAY_TYPE_KEY, DEFAULT_SCORE_DISPLAY_TYPE) {
-            ScoreDisplayType.valueOf(it)
-        }
         val openLastArchiveOnStartup = getFromSourceOrDefault(
             OPEN_LAST_ARCHIVE_ON_STARTUP_KEY, DEFAULT_OPEN_LAST_ARCHIVE_ON_STARTUP
         )
@@ -78,16 +73,28 @@ class AppSettingsServiceImpl : AppSettingsService {
         }
         val checkUpdates = getFromSourceOrDefault(CHECK_UPDATES_KEY, DEFAULT_CHECK_UPDATES)
         val ignoreVersion = getFromSourceOrNull<String>(IGNORE_VERSION_KEY)
+
+        val themeBehavior = getFromSourceAndParseOrDefault(THEME_BEHAVIOR_KEY, DEFAULT_THEME_BEHAVIOR) {
+            ThemeBehavior.valueOf(it)
+        }
+        val scoreDisplayType = getFromSourceAndParseOrDefault(SCORE_DISPLAY_TYPE_KEY, DEFAULT_SCORE_DISPLAY_TYPE) {
+            ScoreDisplayType.valueOf(it)
+        }
+
+        val showForeignImportSources = getFromSourceOrDefault(
+            SHOW_FOREIGN_IMPORT_SOURCES_KEY, DEFAULT_SHOW_FOREIGN_IMPORT_SOURCES
+        )
         val ponyIntegrations = getFromSourceOrDefault(PONY_INTEGRATIONS_KEY, DEFAULT_ENABLE_PONY_INTEGRATIONS)
 
         current = AppSettings(
             locale = locale,
-            themeBehavior = themeBehavior,
-            scoreDisplayType = scoreDisplayType,
             openLastArchiveOnStartup = openLastArchiveOnStartup,
             recentlyOpenedArchives = recentlyOpenedArchives,
             checkUpdates = checkUpdates,
             ignoreVersion = ignoreVersion,
+            themeBehavior = themeBehavior,
+            scoreDisplayType = scoreDisplayType,
+            showForeignImportSources = showForeignImportSources,
             enablePonyIntegrations = ponyIntegrations,
         )
     }
@@ -137,26 +144,32 @@ class AppSettingsServiceImpl : AppSettingsService {
     private fun updateToSource() {
         current.let {
             settingsSource[LOCALE_KEY] = it.locale?.toTag()
-            settingsSource[THEME_BEHAVIOR_KEY] = it.themeBehavior.name
-            settingsSource[SCORE_DISPLAY_TYPE_KEY] = it.scoreDisplayType.name
             settingsSource[OPEN_LAST_ARCHIVE_ON_STARTUP_KEY] = it.openLastArchiveOnStartup
             settingsSource[RECENTLY_OPENED_ARCHIVES_KEY] = AppJson.encodeToString(
                 ListSerializer(PathSerializer), it.recentlyOpenedArchives
             )
             settingsSource[CHECK_UPDATES_KEY] = it.checkUpdates
             settingsSource[IGNORE_VERSION_KEY] = it.ignoreVersion
+
+            settingsSource[THEME_BEHAVIOR_KEY] = it.themeBehavior.name
+            settingsSource[SCORE_DISPLAY_TYPE_KEY] = it.scoreDisplayType.name
+
+            settingsSource[SHOW_FOREIGN_IMPORT_SOURCES_KEY] = it.showForeignImportSources
             settingsSource[PONY_INTEGRATIONS_KEY] = it.enablePonyIntegrations
         }
     }
 
     companion object {
         private const val LOCALE_KEY = "Locale"
-        private const val THEME_BEHAVIOR_KEY = "ThemeBehavior"
-        private const val SCORE_DISPLAY_TYPE_KEY = "ScoreDisplayType"
         private const val OPEN_LAST_ARCHIVE_ON_STARTUP_KEY = "OpenLastArchiveOnStartup"
         private const val RECENTLY_OPENED_ARCHIVES_KEY = "RecentlyOpenedArchives"
         private const val CHECK_UPDATES_KEY = "CheckUpdates"
         private const val IGNORE_VERSION_KEY = "IgnoreVersion"
+
+        private const val THEME_BEHAVIOR_KEY = "ThemeBehavior"
+        private const val SCORE_DISPLAY_TYPE_KEY = "ScoreDisplayType"
+
+        private const val SHOW_FOREIGN_IMPORT_SOURCES_KEY = "ShowForeignImportSources"
         private const val PONY_INTEGRATIONS_KEY = "EnablePonyIntegrations"
     }
 }
